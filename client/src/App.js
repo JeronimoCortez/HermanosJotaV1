b93b8db0f11a197e679d96c16cee143b511ddbd6
Fausto Leal <fausto.leal2099@gmail.com>
2025-09-28 21:35:10 -0300

    agregando contenido al App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import ProductPage from "../src/components/screen/ProductPage";
+import { useState } from "react";
+import Navbar from "./components/ui/navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  const [id, setId] = useState(undefined);
+  const [pagina, setPagina] = useState("inicio");
+  return (
+    <div className="App">
+      <Navbar setPagina={setPagina} />
+      {pagina === "inicio" && <h1>Pagina de inicio</h1>}
+      {pagina === "productos" && (
+        <ProductPage setId={setId} setPagina={setPagina} />
+      )}
+      {pagina === "detalle" && <h1>{`Viendo producto ${id}`}</h1>}
+      {pagina === "contacto" && <h1>Pagina de contacto</h1>}
+    </div>
+  );
 }
 
 export default App;
